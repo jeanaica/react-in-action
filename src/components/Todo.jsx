25e054c503a2e8856d2e68b02788dbe51cd361b2
Jeanaica Suplido-Alinsub <sjeanaica@gmail.com>
2023-07-06 12:26:57 +0800

    🎨 (Lesson 30): Add event handlers

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,15 @@
 function Todo(props) {
+  const handleDelete = () => {
+    console.log('Clicked!');
+  };
+
   return (
     <div className='card'>
       <h2>{props.text}</h2>
       <div className='actions'>
-        <button className='btn'>Delete</button>
+        <button className='btn' onClick={handleDelete}>
+          Delete
+        </button>
       </div>
     </div>
   );
