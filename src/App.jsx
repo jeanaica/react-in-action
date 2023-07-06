42abda17d45d0a66750f14d436c7fa3ac9c1c61e
Jeanaica Suplido-Alinsub <sjeanaica@gmail.com>
2023-07-06 12:15:43 +0800

    ✨ (Lesson 29): Passing props to components

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo />
+      <Todo text='Learn React' />
+      <Todo text='Read' />
+      <Todo text='Study' />
     </div>
   );
 }
