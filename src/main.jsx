08a2fcfa0411841405e02ccc36c325c3b12156ad
Jeanaica Suplido-Alinsub <sjeanaica@gmail.com>
2023-07-13 10:36:39 +0800

    🚀 (Deployment): Fix 404 error

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import { analytics, firebaseApp, setupAppCheck } from '../firebase';
 import App from './App.jsx';
 import { FavoritesContextProvider } from './store/favorites-context';
 import './index.css';
-
-const app = firebaseApp();
-
-setupAppCheck(app);
-analytics(app);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
